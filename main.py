9852a066c0b8680d35f9590f91cd2700f14cf1fc
Matthew K <Matthew.Kubyshkin@gmail.com>
2025-08-13 13:00:27 -0400

    Added model NN class, inheriting PyTorch base model. Added constructor in model class, additionally calling the parent nn constructor.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import torch
 import os
-from torch import nn
+from torch import nn # nn -- neural network 
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import torch.nn.functional as F
+
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 print("Torch Version: ", torch.__version__)
 print("CUDA Available:", torch.cuda.is_available())
+
+class NeuralNetwork(nn.Module) :
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.flatten = nn.Flatten()
+        
