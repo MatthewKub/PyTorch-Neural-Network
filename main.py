85744d17da2ed68a8104698367084b231364f8f3
Matthew K <Matthew.Kubyshkin@gmail.com>
2025-08-12 23:23:48 -0400

    Imported packages, modules, and submodules. Added training device verification.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import torch;
+import torch
+import os
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
-print("Torch Version: ", torch.__version__);
-print("CUDA Available:", torch.cuda.is_available());
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
+print("Torch Version: ", torch.__version__)
+print("CUDA Available:", torch.cuda.is_available())
